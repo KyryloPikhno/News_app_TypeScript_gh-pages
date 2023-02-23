6b79eefa9975dd2c53fd812a78d21ec0fe26abe7
KyryloPikhno <Mr.Good@i.ua>
2023-02-23 14:47:38 +0200

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React, {FC} from "react";
 import {Route, Routes} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 
 import {HomePage, LoginPage, NewsDetailsPage, NewsPage, ProfilePage} from "./pages";
 import {MainLayout} from "./layouts";
 import {PrivateRoute} from "./utils";
 
 
-
 const App: FC = () => {
 
 
-
     return (
-        <Routes>
-            <Route path={'/'} element={<MainLayout/>}>
-                <Route path={'/login'} element={<LoginPage/>}/>
-                <Route path={'/'} element={<HomePage/>}/>
-                <Route path={'/news'} element={<NewsPage/>}/>
-                <Route path={'/news/:id'} element={<NewsDetailsPage/>}/>
-                <Route path={'/profile'} element={<PrivateRoute><ProfilePage/></PrivateRoute>}/>
-            </Route>
-        </Routes>
+        <BrowserRouter basename="/news">
+            <Routes>
+                <Route path={'/'} element={<MainLayout/>}>
+                    <Route path={'/login'} element={<LoginPage/>}/>
+                    <Route path={'/'} element={<HomePage/>}/>
+                    <Route path={'/news'} element={<NewsPage/>}/>
+                    <Route path={'/news/:id'} element={<NewsDetailsPage/>}/>
+                    <Route path={'/profile'} element={<PrivateRoute><ProfilePage/></PrivateRoute>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import ReactDOM from 'react-dom/client';
 import React from 'react';
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
