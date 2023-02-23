f324df27061b73ef6be06c8a810559c566af004f
KyryloPikhno <Mr.Good@i.ua>
2023-02-23 14:57:02 +0200

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, {FC} from "react";
 import {Route, Routes} from "react-router";
-import {BrowserRouter} from "react-router-dom";
 
 import {HomePage, LoginPage, NewsDetailsPage, NewsPage, ProfilePage} from "./pages";
 import {MainLayout} from "./layouts";
@@ -11,7 +10,6 @@ const App: FC = () => {
 
 
     return (
-        <BrowserRouter basename="/news">
             <Routes>
                 <Route path={'/'} element={<MainLayout/>}>
                     <Route path={'/login'} element={<LoginPage/>}/>
@@ -21,7 +19,6 @@ const App: FC = () => {
                     <Route path={'/profile'} element={<PrivateRoute><ProfilePage/></PrivateRoute>}/>
                 </Route>
             </Routes>
-        </BrowserRouter>
     );
 };
 
